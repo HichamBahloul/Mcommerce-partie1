faffafd991cd8c3fa10ad6bf66e6ddcc94bd72ee
hicham <hicham.tr2@gmail.com>
2019-02-22 19:15:20 +0000

    Ajout de la methode calculerMargeProduit , qui calcule la marge de chaque produit

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,22 +1,33 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.List;
 
 
 @Api( description="API pour es opérations CRUD sur les produits.")
@@ -61,9 +72,7 @@ public class ProductController {
         return produit;
     }
 
-
-
-
+   
     //ajouter un produit
     @PostMapping(value = "/Produits")
 
@@ -102,7 +111,23 @@ public class ProductController {
 
         return productDao.chercherUnProduitCher(400);
     }
-
+//==============================================================//
+    @ApiOperation(value = "calcule la marge de chaque produit")
+    @GetMapping(value = "/AdminProduits")
+    public String calculerMargeProduit() {
+        int marge;
+        String s="";
+    	
+    	
+    	List<Product> produits = productDao.findAll();
+        for (Product product : produits) {
+			marge=product.getPrix()-product.getPrixAchat();
+			s+="\""+product.toString()+" :"+marge+"\""+", \n";
+		}
+        
+        return s;
+    }
+//=============================================================//
 
 
 }
